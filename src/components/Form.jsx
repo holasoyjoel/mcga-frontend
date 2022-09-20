e7c00fbae764305659d7a824ca01a7be779cf2cf
Correa Joel <joelcorrea.alejandro123@gmail.com>
2022-09-19 22:30:37 -0300

    primer commit

---
+++ src/components/Form.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from 'react'
+
+
+const Form = () => {
+    const [usuario , setUsuario] = useState("");
+    const [password , setPassword] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(`${usuario} - ${password}`);
+        localStorage.setItem("usuario" , JSON.stringify(usuario));
+        localStorage.setItem("password" , JSON.stringify(password));
+        setUsuario("");
+        setPassword("");
+    }
+    
+    const handleInputChangeUsuario = (e) =>{
+        setUsuario(e.target.value);
+        
+    }
+    const handleInputChangePassword = (e) =>{
+        setPassword(e.target.value);
+    }
+    return (
+        <form onSubmit={handleSubmit}>
+            <label>Usuario</label>
+            <input 
+                type="text"
+                name='usuario'
+                onChange={handleInputChangeUsuario}
+                required
+                value={usuario}
+            />
+            
+            <label>Contraseña</label>
+            <input 
+                type="password"
+                name='contraseña'
+                onChange={handleInputChangePassword}
+                required
+                value={password}
+            />
+            <button type='submit'>Enviar</button>
+        </form>
+    )
+}
+
+export default Form;
